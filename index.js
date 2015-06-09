d8165c4d25dc4149f4f57a193756dad51961c81c
kirby81 <etienne.ellie@gmail.com>
2015-06-09 14:15:37 +0200

    add room

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var roomlist = ['general'];
+
 app.use(express.static(__dirname + '/view'));
 
 app.get('/', function(req, res) {
@@ -23,6 +25,7 @@ io.on('connection', function (socket) {
     socket.emit('res', 'SERVER', 'you have connected to general');
     socket.broadcast.to(socket.room).emit('res', 'SERVER', username + ' has connected to this room');
     socket.emit('updateroom', socket.room);
+    socket.emit('roomlist', roomlist);
   });
 
   /**
@@ -37,6 +40,23 @@ io.on('connection', function (socket) {
    */
   socket.on('msg', function (msg) {
     io.sockets.in(socket.room).emit('res', socket.username, msg);
+  });
+
+  /**
+   ** Un utilisateur crée un salon
+  */
+  socket.on('newroom', function (room) {
+    roomlist.push(room);
+    socket.emit('updateroom', room);
+    io.sockets.emit('roomlist', roomlist);
+  })
+
+  /**
+   ** Un utilisateur change de salon
+  */
+  socket.on('changeroom', function (room) {
+    socket.room = room;
+    socket.emit('updateroom', room);
   });
 
 })
--- view/js/chat.js
+++ view/js/chat.js
@@ -21,14 +21,35 @@ $(document).ready(function () {
     return (false);
   });
 
+  //Changement de room
+  $('#roomlist').click(function () {
+    alert("plop");
+    //socket.emit('changeroom', event.target.text());
+  });
+
+  //Creation d'un nouveau salon
+  $('#addroom').click(function () {
+    var room = prompt("Room name");
+    if (room)
+      socket.emit('newroom', room);
+  });
+
   //Reception message serveur
   socket.on('res', function (user, res) {
     $('#chat').append($('<li>').text(user + ': ' + res));
   });
 
+  //Ajout d'un salon part un utilisateur
+  socket.on('roomlist', function (list) {
+    $('li').remove('#roomlist');
+    for (var i = 0; i < list.length; i++) {
+      $('.dropdown-menu').prepend('<li id="roomlist"><a href="#">' + list[i] + '</a></li>');
+    }
+  });
+
   //Update de la room du chat
   socket.on('updateroom', function (nameroom) {
-    $('#room').val('');
+    $('#room').empty();
     $('#room').append(nameroom);
   });
 
